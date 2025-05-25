3412b319dd5b10f2d718f381174b3f2bce4a5844
bdvzt <budaevazay@mail.ru>
2025-05-25 10:38:37 +0700

    Inital commit

---
+++ src/main/java/com/example/ctf_kt_0/CtfKt0Application.java
@@ -0,0 +1,13 @@
+package com.example.ctf_kt_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtfKt0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtfKt0Application.class, args);
+    }
+
+}
